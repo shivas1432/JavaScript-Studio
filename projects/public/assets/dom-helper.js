c940a2b5aa22ec5ab09ecce9381899f1f3e09663
shivas1432 <shivashanker7337@gmail.com>
2024-08-19 09:45:00 +0100

    Add DOM utilities

---
+++ projects/public/assets/dom-helper.js
@@ -0,0 +1,9 @@
+﻿// DOM Helper
+const DOMHelper = {
+    createElement(tag, className = '') {
+        const element = document.createElement(tag);
+        if (className) element.className = className;
+        return element;
+    }
+};
+window.DOMHelper = DOMHelper;
