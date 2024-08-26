08b9befb8117aae8e7506e94eff31aeb68d8e17f
shivas1432 <shivashanker7337@gmail.com>
2024-08-26 16:20:00 +0100

    Add validation utilities

---
+++ projects/public/assets/validator.js
@@ -0,0 +1,8 @@
+﻿// Validator
+const Validator = {
+    isEmail(email) {
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return emailRegex.test(email);
+    }
+};
+window.Validator = Validator;
